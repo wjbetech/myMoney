6064c48ae20258aca83c9a4c86017ff4a09ec440
William East <wjbetech@gmail.com>
2023-11-09 17:14:46 +0900

    Login page front side rendering working

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,40 @@
 import React from "react";
 import styles from "./Login.module.css";
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  // set state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // handlers
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form action="" className={styles["login.form"]} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 };
 
 export default Login;
